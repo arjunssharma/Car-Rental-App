bab748003672cc96ad5a0cba5518b81cd7d96c3a
gjoshi <gjoshi@ncsu.edu>
2017-09-13 17:32:09 -0400

    Adding front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'car_rental#home'
+  #get '/home', to: 'car_rental/home'
+  get '/help', to: 'car_rental#help'
+  get '/about', to:'car_rental#about'
+  get  '/signup',  to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'users#index'
 end
