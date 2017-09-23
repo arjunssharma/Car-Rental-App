5152a5883b8a0f93de7ad73343bdba9154f25a97
Arjun Sharma <asharm33@ncsu.edu>
2017-09-23 16:40:21 -0400

    committing DB and Gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Car.create!(name: 'Mercedes-Benz S550', comfort_class: 'A', description: 'Year: 2015, Engine: 4.7, Horsepower: 449, Fuel: Gasoline')
+
+Car.create!(name: 'Volkswagen Passat', comfort_class: 'B', description: 'Year: 2014, Engine: 1.4, Horsepower: 150, Fuel: Gasoline')
+
+Car.create!(name: 'Fiat Panda', comfort_class: 'C', description: 'Year: 2014, Engine: 1.1, Horsepower: 54, Fuel: Gasoline')
+
+4.times do
+  3.times do
+    name = Faker::Name.last_name
+    content = Faker::Lorem.sentence(5)
+
+    Car.create!(name:  name,
+                comfort_class: 'B',
+                description: content
+    )
+  end
+  3.times do
+    name = Faker::Name.last_name
+    content = Faker::Lorem.sentence(5)
+
+    Car.create!(name:  name,
+                comfort_class: 'C',
+                description: content
+    )
+  end
+  3.times do
+    name = Faker::Name.last_name
+    content = Faker::Lorem.sentence(5)
+
+    Car.create!(name:  name,
+                comfort_class: 'A',
+                description: content
+    )
+  end
+end
+
+cars = Car.order(:created_at).take(20)
+cars.each do |car|
+  name  = Faker::Name.name
+  phone = Faker::PhoneNumber.cell_phone
+  starting = Faker::Date.between(Date.tomorrow, 1.year.from_now)
+  ending = Faker::Date.between(starting + 1, starting + 1.month)
+  car.bookings.create!(starting: starting, ending: ending, client: name, place: 'New York, 55 East 52nd Street', phone: phone)
+end
