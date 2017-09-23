1574c01fcd6139a206659a9f1ea499e4412cbcce
Arjun Sharma <asharm33@ncsu.edu>
2017-09-23 16:36:51 -0400

    committing booking and car

---
+++ app/models/car.rb
@@ -0,0 +1,32 @@
+class Car < ApplicationRecord
+  has_many :bookings
+  mount_uploader :picture, PictureUploader
+  before_save :set_price
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :comfort_class, presence: true
+  validates :description, presence: true, length: { maximum: 100 }
+  validate  :picture_size
+
+  def available?
+    bookings.empty? || bookings.map(&:starting).min > Date.tomorrow
+  end
+
+  private
+
+  def set_price
+    if comfort_class == 'A'
+      self.price = 100
+    elsif comfort_class == 'B'
+      self.price = 75
+    elsif comfort_class == 'C'
+      self.price = 50
+    end
+  end
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, 'should be less than 5MB')
+    end
+  end
+
+end
